ce983573cf0c392f5c45759b5d5cd44f39857fc0
SylvainJunca <sylvain.junca@gmail.com>
2018-10-26 21:10:40 -0400

    Finishes the last pieces of the brut solution. Working

--- stock-market.js
+++ stock-market.js
@@ -6,8 +6,14 @@ function maxProfit(arr) {
           bruteData.push(arr[j+1] - arr[i]);
         }
     }
-    return bruteData;
+    bruteData.sort((a, b) => a - b);
+    var mProfit = bruteData.length - 1;
+    if(bruteData[mProfit] > 0) {
+        return bruteData[mProfit];
+    }
+    return -1;
 }
 
 
-console.log(maxProfit([45, 24, 35, 31, 40, 38, 11])); 
+console.log(maxProfit([45, 24, 35, 31, 40, 38, 11]));
+console.log(maxProfit([45, 24, 23, 21, 19, 8, 4]));
